fafae7794430ed2091eaed91e5c054058de9f122
aleksander <aleksander.pawlak@op.pl>
2024-06-27 13:54:32 +0200

    Project details initial structure and design

--- src/components/SelectedProject.jsx
+++ src/components/SelectedProject.jsx
@@ -1,31 +1,33 @@
 import classes from '../css/SelectedProject.module.css'
 import BackButton from './BackButton';
 
-export default function SelectedProject( { project, onBackBtn } ) {
+export default function SelectedProject({ project, onBackBtn }) {
     return (
         <>
-            <BackButton onClick={onBackBtn}/>
+            <BackButton onClick={onBackBtn} />
             <div className={classes['content-container']}>
-                <div className={classes.header}>
-                    <h1>{project.title} TITLE</h1>
-                    <button>Delete</button>
-                </div>
-                <div className={classes['project-details']}>
-                    <p>{project.date} DATE</p>
-                    <p>{project.description} DESCR</p>
-                </div>
-                <hr></hr>
-                <div className={classes['tasks-container']}>
-                    <div className={classes['add-box']}>
-                        <h3>Tasks</h3>
-                        <div className={classes['task-input']}>
-                            <input type="text"></input>
-                            <button>Add Task</button>
+                <div className={classes['content-box']}>
+                    <div className={classes['header-box']}>
+                        <div className={classes.header}><h1>{project.title}</h1></div>
+                        <div className={classes.btn}><button>Delete</button></div>
+                    </div>
+                    <div className={classes['project-details']}>
+                        <p>Due: {project.date}</p>
+                        <p>{project.description}</p>
+                    </div>
+                    <hr></hr>
+                    <div className={classes['tasks-container']}>
+                        <div className={classes['add-box']}>
+                            <h3>Tasks</h3>
+                            <div className={classes['task-input']}>
+                                <input type="text"></input>
+                                <button>Add Task</button>
+                            </div>
+                            <p>This project does not have any tasks yet.</p>
                         </div>
-                        <p>This project does not have any tasks yet.</p>
                     </div>
                 </div>
             </div>
         </>
-    );    
+    );
 }
